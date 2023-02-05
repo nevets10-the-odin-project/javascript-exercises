91d237b48fde63753f069acf6fe4799fedb2e89f
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-04 20:28:06 -0800

    Update tempConversion functions, pass second test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const convertToCelsius = function (fahrenheit) {
 	const celsius = (fahrenheit - 32) * (5 / 9);
 	if (Number.isInteger(celsius)) return celsius;
-	return celsius.toFixed(1);
+	return +celsius.toFixed(1);
 };
 
 const convertToFahrenheit = function (celsius) {
 	const fahrenheit = (celsius * (5 / 9) + 32).toFixed(1);
 	if (Number.isInteger(fahrenheit)) return fahrenheit;
-	return fahrenheit.toFixed(1);
+	return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
