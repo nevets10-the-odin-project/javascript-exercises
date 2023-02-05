42cf0700868bde802a87a84730adf2b93a499840
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-04 19:07:39 -0800

    Write removeFromArray function, pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...values) {
+	for (const value of values) {
+		if (array.indexOf(value) !== -1) {
+			array.splice(array.indexOf(value), 1);
+		}
+	}
+	return array;
 };
 
 // Do not edit below this line
