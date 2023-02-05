d0a2082655059169c1625b03d4c0ee7e78d0a409
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-04 20:09:02 -0800

    Refactor, pass last test, complete exercise
    Realized that I didn't need a variable and could just return the result
    directly. My solution actually matches the proposed solution exactly,
    barring the indentation.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,5 @@
 const leapYears = function (year) {
-	const isLeapYear = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
-	return isLeapYear;
+	return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
