1b61ecbeeef09266868c3ab7d73540f3ae688929
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-04 19:49:24 -0800

    Correct number guard clause

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function (num1, num2) {
-	if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+	if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
 	if (num1 < 0 || num2 < 0) return "ERROR";
 
 	let smallerNumber = num1 <= num2 ? num1 : num2;
