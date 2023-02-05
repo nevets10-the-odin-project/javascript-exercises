d98d21e0a326523c6ec6f2d4057bd0c08924d86d
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-04 19:32:08 -0800

    Fill out sumAll function, pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
 
+	let smallerNumber = num1 <= num2 ? num1 : num2;
+	const largerNumber = num1 >= num2 ? num1 : num2;
+	let sum = 0;
+
+	while (smallerNumber <= largerNumber) {
+		sum += smallerNumber;
+		smallerNumber++;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
