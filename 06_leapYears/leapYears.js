324f7160a91de7987d3d114cf005eb3a27392d21
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-04 20:05:20 -0800

    Fill out leapYears function, pass first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+	const isLeapYear = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+	return isLeapYear;
 };
 
 // Do not edit below this line
