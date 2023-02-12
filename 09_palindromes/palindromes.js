5931ff82b054166d6e7258e8ccc512d2bf9a9b1c
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-11 20:19:05 -0800

    Correct palindromes function, pass second test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,7 @@
 const palindromes = function (string) {
-	const reversed = string
-		.replaceAll(/[^\w]/g, "")
-		.toLowerCase()
-		.split("")
-		.reverse()
-		.join("");
-
-	return string === reversed;
+	const filteredString = string.replaceAll(/[^\w]/g, "").toLowerCase();
+	const reversed = filteredString.split("").reverse().join("");
+	return filteredString === reversed;
 };
 
 // Do not edit below this line
