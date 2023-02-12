efcc3f66364be036d24eb0ee8dfd0f0e71706d71
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-11 20:44:45 -0800

    Populate fibonacci function, pass first test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+	const φ = (1 + Math.sqrt(5)) / 2;
+	return Math.floor(φ ** number / Math.sqrt(5) + 1 / 2);
 };
 
 // Do not edit below this line
