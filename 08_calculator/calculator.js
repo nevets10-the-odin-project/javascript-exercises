1bdd420c98d7e1e1ea4928f5451fa5fa8a4ec5c0
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-11 19:28:19 -0800

    Pass subtract test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,7 +2,9 @@ const add = function (num1, num2) {
 	return num1 + num2;
 };
 
-const subtract = function () {};
+const subtract = function (num1, num2) {
+	return num1 - num2;
+};
 
 const sum = function () {};
 
