d1487d52595ab93f103a89e166ceb63915c130f0
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-11 22:05:28 -0800

    Refactor findTheOldest
    After reviewing the proposed solution, it was obvious that getting the
    age could be generalized into a new function.
    
    I was also able to shave a whopping two lines off of the proposed
    solution, which probably doesn't make it any better.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,13 @@
 const findTheOldest = function (people) {
-	return people.reduce((oldest, person) => {
-		if (!oldest) return person;
+	return people.reduce((oldest, nextPerson) => {
+		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const nextPersonAge = getAge(nextPerson.yearOfBirth, nextPerson.yearOfDeath);
+		return oldestAge > nextPersonAge ? oldest : nextPerson;
+	});
+};
 
-		const currentYear = new Date().getFullYear();
-
-		const oldestMaxYear =
-			"yearOfDeath" in oldest ? oldest.yearOfDeath : currentYear;
-		const oldestAge = oldestMaxYear - oldest.yearOfBirth;
-
-		const personMaxYear =
-			"yearOfDeath" in person ? person.yearOfDeath : currentYear;
-		const personAge = personMaxYear - person.yearOfBirth;
-
-		return oldestAge > personAge ? oldest : person;
-	}, {});
+const getAge = function (yearOfBirth, yearOfDeath = new Date().getFullYear()) {
+	return yearOfDeath - yearOfBirth;
 };
 
 // Do not edit below this line
