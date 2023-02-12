338d4a3b03a0044fae4b647fe5dd005eb43e60d4
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-11 21:21:58 -0800

    Populate findTheOldest function, pass first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	return people.reduce((oldest, person) => {
+		if (!oldest) return person;
 
+		return oldest.yearOfDeath - oldest.yearOfBirth >
+			person.yearOfDeath - person.yearOfBirth
+			? oldest
+			: person;
+	}, {});
 };
 
 // Do not edit below this line
