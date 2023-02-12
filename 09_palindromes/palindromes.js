3ae7bdb6571edf4f4e9d298362685602bfe9bc5b
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-11 20:14:32 -0800

    Populate palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	const reversed = string
+		.replaceAll(/[^\w]/g, "")
+		.toLowerCase()
+		.split("")
+		.reverse()
+		.join("");
 
+	return string === reversed;
 };
 
 // Do not edit below this line
