d0029fe74a61e339989b813042f4bc5c4b273b21
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-11 19:31:07 -0800

    Pass first sum test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,7 +6,13 @@ const subtract = function (num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function () {};
+const sum = function (array) {
+	let sum = 0;
+	for (number in array) {
+		sum += number;
+	}
+	return sum;
+};
 
 const multiply = function () {};
 
